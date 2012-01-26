d2dc30eb9651d00e3af7f96a40f3c9c1f2b852d6
kailash kumar <kailashk06@gmail.com>
2012-01-26 13:03:39 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gither::Application.config.secret_token = '79d5c24d5b57cb9c6bb1241af48aef6ab5eaddd946423bb99c686daf89a40a4bba11df2140d3d22f68da718f41d31f5c3f0912d2342041b06a92a0502fdcd091'
